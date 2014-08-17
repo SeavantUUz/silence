82e25b01c6715cb857741ca2db7e6bd2fac00f45
AprocySanae <axdiaoqi220@gmail.com>
2014-08-16 23:47:38 -0600

    add step_second.py

---
+++ prepare/step_second.py
@@ -0,0 +1,39 @@
+import curses
+from curses.textpad import Textbox
+
+def main():
+    stdscr = curses.initscr()
+    curses.nocbreak()
+    curses.curs_set(0)
+    size_y,size_x = stdscr.getmaxyx()
+    
+    # curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    # curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    stdscr.hline(size_y-2,0,ord('-'),size_x)
+    stdscr.move(size_y-1,0)
+    content = []
+    line = ""
+    # input_y, input_x = size_y-3,0
+    while True:
+        ch = stdscr.getch()
+        if ch == curses.KEY_ENTER or ch == 10:
+            stdscr.clear()
+            stdscr.hline(size_y-2,0,ord('-'),size_x)
+            stdscr.refresh()
+            content.append(line)
+            line = ""
+            show_content(stdscr, content)
+            stdscr.move(size_y-1,0)
+        else:
+            line += chr(ch)
+
+def show_content(stdscr, content):
+    stdscr.move(0,0)
+    for y in xrange(len(content)):
+        stdscr.addstr(y,0,content[y])
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        curses.endwin()
